e27389984498ecd0ce78e6f4306c360baa8262d9
Pierre Merlin <pierrem@apido.org>
2014-02-10 22:49:42 +0100

    Update canonical.php

--- tests/phpunit/tests/canonical.php
+++ tests/phpunit/tests/canonical.php
@@ -98,62 +98,6 @@ class Tests_Canonical extends WP_UnitTestCase {
 		return $can_url;
 	}
 
-	/**
-	 * @dataProvider data
-	 */
-	function test($test_url, $expected, $ticket = 0) {
-		if ( $ticket )
-			$this->knownWPBug( $ticket );
-
-		$ticket_ref = ($ticket > 0) ? 'Ticket #' . $ticket : null;
-
-		if ( is_string($expected) )
-			$expected = array('url' => $expected);
-		elseif ( is_array($expected) && !isset($expected['url']) && !isset($expected['qv']) )
-			$expected = array( 'qv' => $expected );
-
-		if ( !isset($expected['url']) && !isset($expected['qv']) )
-			$this->markTestSkipped('No valid expected output was provided');
-
-		if ( false !== strpos( $test_url, '%d' ) ) {
-			if ( false !== strpos( $test_url, '/?author=%d' ) )
-				$test_url = sprintf( $test_url, $this->author_id );
-			if ( false !== strpos( $test_url, '?cat=%d' ) )
-				$test_url = sprintf( $test_url, $this->term_ids[ $expected['url'] ] );
-		}
-
-		$this->go_to( home_url( $test_url ) );
-
-		// Does the redirect match what's expected?
-		$can_url = $this->get_canonical( $test_url );
-		$parsed_can_url = parse_url($can_url);
-
-		// Just test the Path and Query if present
-		if ( isset($expected['url']) )
-			$this->assertEquals( $expected['url'], $parsed_can_url['path'] . (!empty($parsed_can_url['query']) ? '?' . $parsed_can_url['query'] : ''), $ticket_ref );
-
-		if ( ! isset($expected['qv']) )
-			return;
-
-		// "make" that the request and check the query is correct
-		$this->go_to( $can_url );
-
-		// Are all query vars accounted for, And correct?
-		global $wp;
-
-		$query_vars = array_diff($wp->query_vars, $wp->extra_query_vars);
-		if ( !empty($parsed_can_url['query']) ) {
-			parse_str($parsed_can_url['query'], $_qv);
-
-			// $_qv should not contain any elements which are set in $query_vars already (ie. $_GET vars should not be present in the Rewrite)
-			$this->assertEquals( array(), array_intersect( $query_vars, $_qv ), 'Query vars are duplicated from the Rewrite into $_GET; ' . $ticket_ref );
-
-			$query_vars = array_merge($query_vars, $_qv);
-		}
-
-		$this->assertEquals( $expected['qv'], $query_vars );
-	}
-
 	function data() {
 		/* Data format:
 		 * [0]: $test_url,
